cded333fc54aefc04a67c13fbffe50c08f22095f
Kirill Lavrov <lkv@myttk.ru>
2022-07-11 18:09:43 +0700

    create react template build

---
+++ src/modules/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import img from '../img/image-3.jpg'
+
+const Module = () => (
+  <>
+    <h1>Привет React!!!</h1>
+    <img src={img} />
+  </>
+);
+
+export default Module;
